f9c413a3327498627234344341ad23be2bb01c4e
David Mena <davidmenam15@gmail.com>
2020-11-29 17:54:11 -0500

    Feat: Version 1, program functioning with some problems in the t SNE graph, which will be corrected soon

--- src/Coordinator.py
+++ src/Coordinator.py
@@ -9,9 +9,17 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve, accuracy_score, precision_score, recall_score, roc_auc_score
 from matplotlib import pyplot
-import sklearn.neural_network as nn
 import numpy as np
 import itertools
+from sklearn.manifold import TSNE
+from collections import defaultdict
+# scipy test
+from scipy.cluster.vq import kmeans, vq
+from scipy.spatial.distance import cdist
+
+# personalized import
+# from sklearn.cluster import SpectralClustering
+from SpectralClustering import SpectralClustering
 
 
 # class Coordinator
@@ -30,6 +38,12 @@ class Coordinator:
 
     # normalize data set
     def normalize(self, dataSet):
+        # clean data
+        # dataSet = dataSet.drop('area', axis=1)
+        # dataSet = dataSet.drop('kurtosis', axis=1)
+        # dataSet = dataSet.drop('perimeter', axis=1)
+        dataSet.fillna(method='ffill', inplace=True)
+
         # change pd dataframe data type to float
         cols = dataSet.columns
         for col in cols:
@@ -49,13 +63,16 @@ class Coordinator:
             if tempMax > max:
                 max = tempMax
 
+        print('max ', max)
+        print('min ', min)
+
         # apply min max for each value
         idxRow = 0
         for _, dim in dataSet.iterrows():  # for dimension or attributes
             # avoid normalizing first binary column
             for idxCol, value in dim.items():  # for idx or numerosity
                 dataSet.iat[int(idxRow), int(idxCol)] = round(
-                    (value-min)/(max-min), 6)
+                    (value-min)/(max-min), 12)
             # increment row count
             idxRow += 1
 
@@ -64,268 +81,198 @@ class Coordinator:
         # return
         return dataSet
 
-    # Run one Neural network configurations
-    def runConfig(self, completeDf, k, hidLayers, activFunc):
+    # Run multiple Spectral Clustering config
+    def runConfig(self, completeDf):
         # shuffle
         rowNum, _ = completeDf.shape
         completeDf = completeDf.sample(rowNum, random_state=1)
-        # Target column and other columns of DataSet
-        targ = completeDf.iloc[:, 0].to_numpy().astype(int)
-        features = completeDf.iloc[:, 1:].to_numpy()
-        # MLPClassifier configuracion
-        # topologia
-        hiddenLayers = hidLayers
-        activFunc = activFunc
-        # hiper parametros
-        lrs = [0.01, 0.05, 0.1, 0.3, 0.5]
-        # epochs = [500, 600, 700, 800, 900, 1000]
-        epochs = [5, 10, 15, 20, 25, 30]
+        # Transform data to numpy matrix
+        completeDf = completeDf.to_numpy().astype(float)
+        # Spectral Clustering
+        # metricas
+        kValues = range(2, 11)  # ks from 2 to 10
+        print('kvalues', kValues)
+        kMax = max(kValues)
+        print(kMax)
         # total mean loss per epoch list
-        meanLossList = []
-        higherAuc = 0
-        # loss classified for distinct epochs
-        meanLoss1 = []
-        meanLoss2 = []
-        meanLoss3 = []
-        meanLoss4 = []
-        meanLoss5 = []
-        meanLoss6 = []
-        # generate result dictionary
-        dictNN = {}
-        # configurations
-        dictNN['Configuraciones:'] = 'Topología'
-        dictNN['capas Ocultas:'] = str(hiddenLayers)
-        dictNN['función de Activación:'] = activFunc
-        dictNN['configuraciones:'] = 'Hiperparámetros'
-        # , shuffle=True)
-        # for epoch and learning rate combinations
-        cartesian = itertools.product(epochs, lrs)
+        inertias = []
+        # kmeans results
+        kMeansList = []
+        centroidsList = []
+        avgDist = []
+        # for all configured kValues
         count = 0
-        for epoch, lr in cartesian:
+        for k in kValues:
             # Increment counter
             count += 1
-            # desempeño
-            meanAccNN = []
-            meanPrecNN = []
-            meanRecallNN = []
-            meanRocAucNN = []
-            meanLossNN = []  # loss for every case
 
-            # generate kfold splits
-            kFold = StratifiedKFold(n_splits=k, random_state=1, shuffle=True)
-            splitsIter = kFold.split(features, targ)
-            # run neural network calculations through all folds
-            print(f'iteration {count}\n')
-            for trainIndex, testIndex in splitsIter:
-                # train and test of every fold
-                X_train, X_test = features[trainIndex], features[testIndex]
-                y_train, y_test = targ[trainIndex], targ[testIndex]
-                # Classifier object
-                clf = nn.MLPClassifier(
-                    # verbose=True,
-                    # batch_size=200,
-                    hidden_layer_sizes=hiddenLayers,
-                    # activation func default is relu
-                    activation=activFunc,
-                    early_stopping=False,
-                    solver='sgd',
-                    # learning_rate='constant',  # no change as epochs advance
-                    learning_rate_init=lr,
-                    # max_iter=epoch,
-                    random_state=1,  # To change random seed
-                    # tol=0.001  # minimum improvement in cost (tol)
-                    # number of epochs it was less than tol, to halt the process
-                    # n_iter_no_change=1000
-                )
-                # fit fold data set
-                clf.fit(X_train, y_train)
-                lossVals = clf.loss_curve_
-                # binary prediction
-                y_pred = clf.predict(X_test)
-                # probabilistic prediction
-                y_prob = clf.predict_proba(X_test)
-                # print('ypred, ', y_pred)
-                # print('ytest, ', y_test)
-                # print('yprob', y_prob)
-                # DESEMPEÑO
-                meanAccNN.append(accuracy_score(y_test, y_pred))
-                meanPrecNN.append(precision_score(y_test, y_pred))
-                meanRecallNN.append(recall_score(y_test, y_pred))
-                meanRocAucNN.append(roc_auc_score(y_test, y_prob[:, 1]))
-                meanLossNN.append(sum(
-                    lossVals) / len(lossVals))
-                # meanLossNN.append(clf.loss_)
-                # print(accuracy_score(y_test, y_pred))
-                # print(precision_score(y_test, y_pred))
-                # print(recall_score(y_test, y_pred))
-                # print(roc_auc_score(y_test, y_prob[:, 1]))
-                print(clf.loss_)
-                # accuracy_score(y_test, y_pred)
-                # print('acc ', roc_auc_score(y_test, y_prob[:, 0]))
-                # print('prec ', precision_score(y_test, y_pred))
-                # print('roc auc score ', roc_auc_score(y_test, y_prob[:, 1]))
-                # print(clf.classes_)
-            # ESCRIBIR RESULTADOS por cada epoch y lr
-            # DESEMPEÑO
-            # metrics to average
-            if(meanAccNN):  # avoid case when folds end
-                # print('im in!')
-                dictNN[f'\nEPOCH and Lr {count}:'] = str(epoch) + '/' + str(lr)
-                dictNN[f'accuracy promedio {count}:'] = sum(
-                    meanAccNN) / len(meanAccNN)
-                dictNN[f'precision promedio {count}:'] = sum(
-                    meanPrecNN) / len(meanPrecNN)
-                dictNN[f'recall promedio {count}:'] = sum(
-                    meanRecallNN) / len(meanRecallNN)
-                meanAUC = sum(
-                    meanRocAucNN) / len(meanRocAucNN)
-                if meanAUC > higherAuc:
-                    higherAuc = meanAUC
-                dictNN[f'roc Auc promedio {count}:'] = meanAUC
-                dictNN[f'Loss promedio {count}:'] = sum(
-                    meanLossNN) / len(meanLossNN)
-                # metrics to desv. est.
-                dictNN[f'accuracy Desv. Est {count}:'] = np.std(meanAccNN)
-                dictNN[f'precision Desv. Est {count}:'] = np.std(meanPrecNN)
-                dictNN[f'recall Desv. Est: {count}'] = np.std(meanRecallNN)
-                dictNN[f'roc Auc Desv. Est: {count}'] = np.std(meanRocAucNN)
-                dictNN[f'loss Desv. Est: {count}'] = np.std(meanLossNN)
+            # run ML Model
+            clf = SpectralClustering(
+                n_clusters=kMax, affinity='rbf')
 
-                # mean of loss y epochs
-                # print('epoch is ', epoch)
-                # print('len bigger loss, ', len(meanLossNN))
-                # print('loss bigger is ', meanLossNN)
-                # print('sum bigger loss is,', sum(meanLossNN))
-                if epoch == epochs[0]:
-                    # print('im in 1')
-                    meanLoss1.append(sum(meanLossNN) / len(meanLossNN))
-                elif epoch == epochs[1]:
-                    # print('im in 2')
-                    meanLoss2.append(sum(meanLossNN) / len(meanLossNN))
-                elif epoch == epochs[2]:
-                    # print('im in 3')
-                    meanLoss3.append(sum(meanLossNN) / len(meanLossNN))
-                elif epoch == epochs[3]:
-                    # print('im in 4')
-                    meanLoss4.append(sum(meanLossNN) / len(meanLossNN))
-                elif epoch == epochs[4]:
-                    # print('im in 5')
-                    meanLoss5.append(sum(meanLossNN) / len(meanLossNN))
-                else:
-                    # print('im in 6')
-                    meanLoss6.append(sum(meanLossNN) / len(meanLossNN))
-        # higher auc
-        dictNN[f'\nHigher AUC of the model:'] = str(higherAuc)
-        # calculate total mean
-        # print('len loss is ', len(meanLoss1))
-        # print('mean loss 1', meanLoss1)
-        # print('mean loss 2', meanLoss2)
-        # print('mean loss 3', meanLoss3)
-        # print('mean loss 4', meanLoss4)
-        # print('mean loss 5', meanLoss5)
-        # print('mean loss 6', meanLoss6)
-        meanLossList.append(sum(meanLoss1) / len(meanLoss1))
-        meanLossList.append(sum(meanLoss2) / len(meanLoss2))
-        meanLossList.append(sum(meanLoss3) / len(meanLoss3))
-        meanLossList.append(sum(meanLoss4) / len(meanLoss4))
-        meanLossList.append(sum(meanLoss5) / len(meanLoss5))
-        meanLossList.append(sum(meanLoss6) / len(meanLoss6))
-        # Return
-        return dictNN, meanLossList, epochs
+            # get model predicted labels
+            # Training the model and Storing the predicted cluster labels
+            # predLabels = clf.fit_predict(completeDf)
+            clf.fit(completeDf)
+            y_pred = clf.labels_
+            inertia = clf.inertia
+            clusters = clf.clusters
+            spectMap = clf.EigVectMap
+            print('spects y total')
+            print(spectMap.shape)
+            print(completeDf.shape)
 
-    # K-fold cross validation
-    def bestConfig(self, completeDf, k, hidLayers, activFunc, epoch, lRate):
+            # save inertia
+            inertias.append(inertia)
+
+            # scipy test
+            km = kmeans(spectMap, k)
+            centroide, _ = km
+            distTemp = cdist(spectMap, centroide, 'euclidean')
+            idx = np.argmin(distTemp, axis=1)
+            dist = np.min(distTemp, axis=1)
+            avgSS = sum(dist) / spectMap.shape[0]
+            print('avgSS', avgSS)
+
+            # append to arrays for graphics
+            kMeansList.append(km)
+            centroidsList.append(centroide)
+            avgDist.append(avgSS)
+        # plot k vs. inertia
+        # self.bestKPlot(kValues, inertias)
+        self.kDistancePlot(kValues, avgDist)
+
+    # Run one Spectral Clustering
+    def run(self, completeDf, k):
         # shuffle
         rowNum, _ = completeDf.shape
         completeDf = completeDf.sample(rowNum, random_state=1)
-        # Target column and other columns of DataSet
-        targ = completeDf.iloc[:, 0].to_numpy().astype(int)
-        features = completeDf.iloc[:, 1:].to_numpy()
+        # Transform data to numpy matrix
+        completeDfNump = completeDf.to_numpy().astype(float)
+        # Spectral Clustering
+        # metricas
+        kVal = k
 
-        # MLPClassifier
-        # Neural Network Metrics
-        # configuracion
-        # topologia
-        hiddenLayers = hidLayers
-        activFunc = activFunc
-        # Hiper parametros
-        lr = lRate
-        epochNum = epoch
-        # Confusion matrix
-        tnNN = 0
-        fpNN = 0
-        fnNN = 0
-        tpNN = 0
-        # VALUES FOR GRAPHICS
-        tprs = []
-        fprs = []
-        precs = []
-        recs = []
-        # generate kFold splits
-        kFold = StratifiedKFold(n_splits=k, random_state=1, shuffle=True)
-        splitsIter = kFold.split(features, targ)
-        # run neural network through all folds
-        for trainIndex, testIndex in splitsIter:
-            # train / test per fold
-            X_train, X_test = features[trainIndex], features[testIndex]
-            y_train, y_test = targ[trainIndex], targ[testIndex]
+        # run ML Model
+        clf = SpectralClustering(
+            n_clusters=kVal, affinity='rbf')
+        # get model predicted labels
+        # Training the model and Storing the predicted cluster labels
+        # predLabels = clf.fit_predict(completeDfNump)
+        clf.fit(completeDfNump)
+        y_pred = clf.labels_
+        inertia = clf.inertia
+        clusters = clf.clusters
+        spectMap = clf.EigVectMap
 
-            # objeto clasificador
-            clf = nn.MLPClassifier(
-                # verbose=True,
-                # batch_size=200,
-                hidden_layer_sizes=hiddenLayers,
-                # activation func default is relu
-                activation=activFunc,
-                early_stopping=False,
-                solver='sgd',
-                # learning_rate='constant',  # no change as epochs advance
-                learning_rate_init=lr,
-                max_iter=epochNum,
-                random_state=1,  # To change random seed
-                # tol=0.001  # minimum improvement in cost (tol)
-                # number of epochs it was less than tol, to halt the process
-                # n_iter_no_change=1000
-            )
-            # fit fold dataset
-            clf.fit(X_train, y_train)
-            # predict binary values
-            y_pred = clf.predict(X_test)
-            # predict probabilistic values
-            y_prob = clf.predict_proba(X_test)
-            # confusion matrix
-            tn, fp, fn, tp = confusion_matrix(
-                y_test, y_pred).ravel()
-            # print('conf matrix')
-            # print('tn ', tn, 'fp ', fp,
-            #       'fn ', fn, 'tp ', tp)
-            tnNN += tn
-            fpNN += fp
-            fnNN += fn
-            tpNN += tp
-            # VALUES FOR GRAPHICS
-            fpr, tpr, _ = roc_curve(
-                y_test, y_prob[:, 1], drop_intermediate=False)
-            precision, recall, _ = precision_recall_curve(
-                y_test, y_prob[:, 1])
-            # print('fpr is', fpr)
-            # print('tpr is', tpr)
-            # print('prec is', precision)
-            # print('rec is', recall)
-            fprs.append(fpr)
-            tprs.append(tpr)
-            precs.append(precision)
-            recs.append(recall)
-        # dictionary result of confussion matrix
-        dictConfMat = {}
-        dictConfMat['tn'] = tnNN
-        dictConfMat['fp'] = fpNN
-        dictConfMat['fn'] = fnNN
-        dictConfMat['tp'] = tpNN
+        # save inertia
+        # plot k vs. inertia
+        # self.bestKPlot(kValues, inertias)
+        colNames = completeDf.columns.values
+        resultGroups = self.bestKPlot(spectMap, y_pred, colNames)
         # return
-        return dictConfMat, fprs, tprs, precs, recs
+        return resultGroups
+
+    # best k plot with tsne
+    def bestKPlot(self, spectralMap, labels, colNames):
+        # Color Mapping
+        # Building the label to color pallette
+        print('spectralMap.shape')
+        print(spectralMap.shape)
+        colDict = {}
+        colDict['blue'] = 'b'
+        colDict['yellow'] = 'y'
+        colDict['green'] = 'g'
+        colDict['red'] = 'r'
+        colDict['cyan'] = 'c'
+        colDict['magenta'] = 'm'
+
+        colors = list(colDict.values())  # , 'k']
+
+        # get only needed colors for features in spectralMap
+        _, numCols = spectralMap.shape
+        colors = colors[:numCols]
+
+        # color chosen pallete
+        print('labels', labels)
+        cPallette = [colors[label] for label in labels]
+        print('cPallette', cPallette)
+
+        # TSNE
+        newMatrix = spectralMap  # np.transpose(spectralMap)
+        newMatrix = TSNE(early_exaggeration=200.0).fit_transform(
+            newMatrix)  # can set rand state
+
+        # figure
+        plt.figure("Best k with tsne Plot")
+
+        # Scatter plot with colors
+        x = newMatrix[:, 0]
+        y = newMatrix[:, 1]
+        print('x', x)
+        print('y', y)
+        plt.scatter(x, y, c=cPallette)
+
+        # show the plot
+        pyplot.show()
+
+        # Place corresponding columns to corresponding clusters
+        resultDict = defaultdict(list)
+        g0 = []
+        g1 = []
+        g2 = []
+        g3 = []
+        g4 = []
+        g5 = []
+        for lab, colN in zip(labels, colNames):
+            if lab == 0:
+                g0.append(colN)
+            if lab == 1:
+                g1.append(colN)
+            if lab == 2:
+                g2.append(colN)
+            if lab == 3:
+                g3.append(colN)
+            if lab == 4:
+                g4.append(colN)
+            else:
+                g5.append(colN)
+        # dictionary result to print in text file
+        colorNames = list(colDict.keys())
+        resultDict[colorNames[0]].extend(g0)
+        resultDict[colorNames[1]].extend(g1)
+        resultDict[colorNames[2]].extend(g2)
+        resultDict[colorNames[3]].extend(g3)
+        resultDict[colorNames[4]].extend(g4)
+        resultDict[colorNames[5]].extend(g5)
+
+        # return
+        return resultDict
+
+        # plot leyend
+        # labcolors = []
+        # labNames = []
+        # count = 0
+        # for colorSelect in colors:
+        #     count += 1
+        #     clr = plt.scatter(x, y, color=colorSelect)
+        #     labcolors.append(clr)
+        #     labNames.append('Class ' + str(count))
+        # plt.legend(tuple(labcolors), tuple(labNames))
+
+    # k vs inertia graphic
+    def kDistancePlot(self, kValues, inertias):
+        pyplot.plot(kValues, inertias, marker='.',
+                    label='K vs. Inertia')
+        pyplot.xlabel('K Values')
+        pyplot.ylabel('Inertia')
+        # show the legend
+        pyplot.legend()
+        # show the plot
+        pyplot.show()
 
     # function to call graphics
+
     def callGraphics(self, fprs, tprs, precs, recs, epochs, meanLossList):
         self.AUCplot(fprs, tprs)
         self.presitionRecallPlot(precs, recs)
--- src/Main.py
+++ src/Main.py
@@ -8,10 +8,7 @@ import pandas as pd
 initPath = r'data\input\initial\DATA.csv'
 # Output
 initNormed = r'data\output\DATA_norm.txt'
-bestDictOut = r'data\output\BestDictionary.txt'
-tablaResOutput = r'data\output\TableResults.txt'
-cartOutput = r'data\output\Cart.txt'
-c45Output = r'data\output\C45.txt'
+resultsPath = r'data\output\Results.txt'
 
 
 # function to write trazability of program to txt file
@@ -33,53 +30,15 @@ def dictToStr(dict):
 coord = Coordinator()
 # joinedDS = coord.join(pathA, pathB)
 # normalize joined Dataset
-initPf = pd.read_csv(initPath, delimiter=',')
+initPf = pd.read_csv(initPath, delimiter=';', encoding="ISO-8859-1")
 normPf = coord.normalize(initPf)
+print('normPf ', str(normPf))
 writeToFile(str(normPf), initNormed, 'w')
 
-# # Call 5 NN configurations
-# k = 10
-# # 1st config
-# hid1 = (16, 8, 6)  # (120, 70, 40, 20)
-# activF1 = 'relu'
-# resDict1, meanLossList1, epochs1 = coord.runConfig(normPf, k, hid1, activF1)
-# strDict1 = '1st Configuration NN:\n' + dictToStr(resDict1)
-# writeToFile(strDict1, tablaResOutput, 'w')
-# # 2nd config
-# hid2 = (30, 18, 10, 4)  # (60, 36, 26, 20, 15)
-# activF2 = 'relu'
-# resDict2, meanLossList2, epochs2 = coord.runConfig(normPf, k, hid2, activF2)
-# strDict2 = '\n\n\n2nd Configuration NN:\n' + dictToStr(resDict2)
-# writeToFile(strDict2, tablaResOutput, 'a')
-# # 3rd config
-# hid3 = (24, 16, 8, 2)  # (88, 62, 33, 12)
-# activF3 = 'tanh'
-# resDict3, meanLossList3, epochs3 = coord.runConfig(normPf, k, hid3, activF3)
-# strDict3 = '\n\n\n3rd Configuration NN:\n' + dictToStr(resDict3)
-# writeToFile(strDict3, tablaResOutput, 'a')
-# # 4th config
-# hid4 = (36, 20, 6)  # (99, 50, 30)
-# activF4 = 'relu'
-# resDict4, meanLossList4, epochs4 = coord.runConfig(normPf, k, hid4, activF4)
-# strDict4 = '\n\n\n4th Configuration NN:\n' + dictToStr(resDict4)
-# writeToFile(strDict4, tablaResOutput, 'a')
-# # 5th config
-# hid5 = (44, 20)  # (108, 20)
-# activF5 = 'tanh'
-# resDict5, meanLossList5, epochs5 = coord.runConfig(normPf, k, hid5, activF5)
-# strDict5 = '\n\n\n5th Configuration NN:\n' + dictToStr(resDict5)
-# writeToFile(strDict5, tablaResOutput, 'a')
+# Run Spectral clustering for k 2 to k 10
+coord.runConfig(normPf.sample(100))
 
-# # Call best configuration
-# hidBest = (24, 16, 8, 2)  # (120, 70, 40, 20)
-# activFBest = 'relu'
-# epochBest = 700
-# lrBest = 0.1
-# bestConfigDict, fprs, tprs, precs, recs = coord.bestConfig(
-#     normPf, k, hidBest, activFBest, epochBest, lrBest)
-# strBestDict = 'Best NN Configuration Confussion Matrix:\n' + \
-#     dictToStr(bestConfigDict)
-# writeToFile(strBestDict, bestDictOut, 'w')
-
-# # Graphs for best configuration
-# coord.callGraphics(fprs, tprs, precs, recs, epochs1, meanLossList1)
+# Run best k spectral clustering
+resultDict = coord.run(normPf.sample(100), 6)
+strResDict = dictToStr(resultDict)
+writeToFile(strResDict, resultsPath, 'w')
